7e2c01117560ec903528e24411f20126552570cb
Burgess <c1023738@hallam.shu.ac.uk>
2021-11-18 11:00:41 +0000

    Added JavaScript Files and Edited Some of them from previous lesson

---
+++ js/exchange.js
@@ -0,0 +1,36 @@
+(function() {
+  var pricePounds;
+  var exchangeRate;
+  var priceEuros;
+
+  var convertForm = document.getElementById("myForm");
+  var msg = document.getElementById("msg");
+  convertForm.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    document.getElementById("pounds").value;
+    priceEuros = 0;
+    exchangeRate = 0.87;
+    pricePounds = parseFloat(document.getElementById("pounds").value);
+    if (isNaN(pricePounds)) {
+      msg.style.display = "block";
+      msg.innerHTML = "You must enter a number";
+      msg.setAttribute("class", "error");
+    }
+    if (pricePounds === 0) {
+      msg.style.display = "block";
+      msg.innerHTML = "You must enter more than zero.";
+      msg.setAttribute("class", "error");
+    } else {
+      priceEuros = pricePounds * exchangeRate;
+      msg.style.display = "block";
+      priceEuros = priceEuros.toFixed(2);
+      msg.innerHTML = "You will get &euro;" + priceEuros;
+      msg.removeAttribute("class");
+    }
+  });
+  document.getElementById("pounds").addEventListener("focus", function() {
+    this.value = "";
+    msg.innerHTML = "";
+    msg.removeAttribute("class");
+  });
+})();
--- js/myScript.js
+++ js/myScript.js
@@ -1,1 +1,83 @@
-console.info("hello from console");
+(function() {
+  //   document.getElementById("myHeading").innerHTML = "Ethan Burgess";
+  //   document.querySelector("nav ul li").setAttribute("class", "currentPage");
+  //   document
+  //     .querySelector("nav ul li a")
+  //     .setAttribute("href", "http://www.google.co.uk ");
+
+  //DOM EVENT
+  //   document.getElementById("myTestEvent").onclick = function() {
+  //     console.info("hi");
+  //   };
+
+  // //  Add EventLisener
+
+  // document.getElementById("myTestEvent").addEventListener("click", myFunction);
+  // function myFunction(ev) {
+  //   console.info(ev.target);
+  //   ev.target.innerHTML("clicked");
+  // }
+
+  // Colour Picker v1
+  // document.querySelector(".red").addEventListener("click", function(ev) {
+  //   document.querySelector("body").setAttribute("class", "redBack");
+  // });
+  // document.querySelector(".green").addEventListener("click", function(ev) {
+  //   document.querySelector("body").setAttribute("class", "greenBack");
+  // });
+
+  // document.querySelector(".blue").addEventListener("click", function(ev) {
+  //   document.querySelector("body").setAttribute("class", "blueBack");
+  // });
+  // document.querySelector(".reset").addEventListener("click", function(ev) {
+  //   document.querySelector("body").setAttribute("class", "colPicker");
+  // });
+
+  //Colour Picker v2
+  var colourButtons = document.querySelectorAll(".colPicker");
+  console.dir(colourButtons);
+  for (var i = 0; i < colourButtons.length; i++) {
+    colourButtons[i].addEventListener("click", chgColour);
+  }
+  function chgColour(ev) {
+    console.dir(ev.target.classList);
+    var colourPicked = ev.target.classList[0] + "Back";
+    document.querySelector("body").setAttribute("class", colourPicked);
+  }
+
+  //
+  // image rotator
+
+  var imageAr = [
+    "images/view1.jpg",
+    "images/view2.jpg",
+    "images/view3.jpg",
+    "images/view4.jpg",
+    "images/view5.jpg",
+    "images/view6.jpg"
+  ];
+  console.dir(imageAr);
+  document.getElementById("myImages").setAttribute("src", imageAr[0]);
+
+  setInterval(chgImg, 4000);
+  var imgNumber = 1;
+  document.getElementById("myTestEvent").onclick = function() {
+    console.info("Called");
+
+    document.getElementById("myImages").setAttribute("src", imageAr[imgNumber]);
+    imgNumber++;
+    if (imgNumber == imageAr.length) {
+      imgNumber = 0;
+    }
+  };
+  function chgImg() {
+    console.info("Called");
+
+    document.getElementById("myImages").setAttribute("src", imageAr[imgNumber]);
+    imgNumber++;
+    if (imgNumber == imageAr.length) {
+      imgNumber = 0;
+    }
+  }
+  chgImg();
+})();
